c358e39da7c3563f3f2a153aaef759d26cc46bfb
sannasichottu <sannasichottu@gmail.com>
2023-12-23 10:05:26 +0530

    Api service for Register

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express()
 app.use(express.json())
 const errorMiddleware = require('./middlewares/error')
 const products = require('./routes/product')
+const auth = require('./routes/auth')
 
 app.use('/api/v1/', products)
+app.use('/api/v1',auth)
 
 app.use(errorMiddleware)
 
