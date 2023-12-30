402ac1787b4ebba7dad093f99ff55c3c5348c288
sannasichottu <sannasichottu@gmail.com>
2023-12-30 12:48:55 +0530

    reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -88,3 +88,29 @@ exports.forgotPassword = catchAsyncError( async (req, res, next)=>{
         }
     
     })  
+
+exports.resetPassword = catchAsyncError(async(req,res,next) => {
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordTokenExpire : {
+            $gt : Date.now()
+        }
+    })
+
+    if(!user) {
+        return next(new ErrorHandler('Password reset token is invalid or expired'))
+    }
+
+    if(req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match'))
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordTokenExpire = undefined;
+    await user.save({validateBeforeSave:false})
+
+    sendToken(user, 201, res)
+})
