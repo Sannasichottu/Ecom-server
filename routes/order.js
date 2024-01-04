895a8499af285435ef58d98f28b4b549b586f2fb
sannasichottu <sannasichottu@gmail.com>
2024-01-04 20:01:42 +0530

    delete order

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { newOrder, getSingleOrder, myOrders, orders, updateOrder } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders, orders, updateOrder, deleteOrder } = require('../controllers/orderController');
 const router = express.Router();
 const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/authenticate')
 
@@ -10,5 +10,6 @@ router.route('/myorders').get(isAuthenticatedUser,myOrders)
 //Admin Routes
 router.route('/orders').get(isAuthenticatedUser, authorizeRoles('admin'),orders)
 router.route('/order/:id').put(isAuthenticatedUser, authorizeRoles('admin'),updateOrder)
+router.route('/order/:id').delete(isAuthenticatedUser, authorizeRoles('admin'),deleteOrder)
 
 module.exports = router;
